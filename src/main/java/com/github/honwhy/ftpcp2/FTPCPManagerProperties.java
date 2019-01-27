ca8994c2ac762c7eca5e02053dd2dcb8143e51dd
Honwhy Wang <honwhy.wang@gmail.com>
2019-01-27 16:58:08 +0800

    change properties key prefix

--- src/main/java/com/github/honwhy/ftpcp2/FTPCPManagerProperties.java
+++ src/main/java/com/github/honwhy/ftpcp2/FTPCPManagerProperties.java
@@ -3,7 +3,7 @@ package com.github.honwhy.ftpcp2;
 import com.github.honwhy.FTPClientManagerConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "ftpcp")
+@ConfigurationProperties(prefix = "spring.ftpcp")
 public class FTPCPManagerProperties extends FTPClientManagerConfig {
 
 }
